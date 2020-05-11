8f1ca7544349ee20adbe74142f844142aadc7479
Michael Guzman <mgchivas4@gmail.com>
2020-05-10 21:43:47 -0500

    changes cases to daily new cases

--- PlotCases.py
+++ PlotCases.py
@@ -8,21 +8,22 @@ statesPath = "us-states.csv"
 
 # County data
 countyData = pd.read_csv(countiesPath)
-cookMask = countyData["county"] == "Cook"
+cookMask = countyData["fips"] == 17031
+print(cookMask)
 countyDates = countyData[cookMask]["date"]
-countyCases = countyData[cookMask]["cases"]
+countyCases = countyData[cookMask]["cases"].diff().fillna(0)
 
 # State data
 stateData = pd.read_csv(statesPath)
 ILMask = stateData["state"] == "Illinois"
 stateDates = stateData[ILMask]["date"]
-stateCases = stateData[ILMask]["cases"]
+stateCases = stateData[ILMask]["cases"].diff().fillna(0)
 
 # plot county data
 plt.bar(countyDates, countyCases)
 plt.xlabel("Date")
 plt.xticks(rotation = 90)
-plt.ylabel("Total Cases")
+plt.ylabel("Daily New Cases")
 plt.title("Total Cases in Cook County")
 
 plt.figure() # create a new figure
@@ -31,7 +32,7 @@ plt.figure() # create a new figure
 plt.bar(stateDates, stateCases)
 plt.xlabel("Date")
 plt.xticks(rotation = 90)
-plt.ylabel("Total Cases")
+plt.ylabel("Daily New Cases")
 plt.title("Total Cases in Illinois")
 
 # plot
