d8a5d18d1dc0b60ec801b4332380d431b5c0acaa
Michael Guzman <mgchivas4@gmail.com>
2020-05-11 10:26:21 -0500

    Added US plot. Minor visual changes

--- PlotCases.py
+++ PlotCases.py
@@ -2,9 +2,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.style.use("seaborn")
 
 countiesPath = "us-counties.csv"
 statesPath = "us-states.csv"
+countryPath = "us.csv"
 
 # County data
 countyData = pd.read_csv(countiesPath)
@@ -20,6 +22,12 @@ stateDates = stateData[ILMask]["date"]
 stateCases = stateData[ILMask]["cases"].diff().fillna(0)
 state7DayAvg = stateCases.rolling(7).mean().fillna(0)
 
+# Country data
+countryData = pd.read_csv(countryPath)
+countryDates = countryData["date"]
+countryCases = countryData["cases"].diff().fillna(0)
+country7DayAvg = countryCases.rolling(7).mean().fillna(0)
+
 # plot county data
 plt.bar(countyDates, countyCases, alpha = .3)
 plt.plot(countyDates, county7DayAvg)
@@ -27,8 +35,11 @@ plt.xlabel("Date")
 plt.xticks(rotation = 90)
 plt.ylabel("Daily New Cases")
 plt.title("Total Cases in Cook County")
+fig = plt.gcf()
+fig.canvas.set_window_title("Cook County")
 
-plt.figure() # create a new figure
+
+plt.figure("Illinois") # create a new figure
 
 # plot state data
 plt.bar(stateDates, stateCases, alpha = .3)
@@ -37,6 +48,16 @@ plt.xlabel("Date")
 plt.xticks(rotation = 90)
 plt.ylabel("Daily New Cases")
 plt.title("Total Cases in Illinois")
+
+plt.figure("United States") # another plot
+
+# plot for country data
+plt.bar(countryDates, countryCases, alpha = .3)
+plt.plot(countryDates, country7DayAvg)
+plt.xlabel("Date")
+plt.xticks(rotation = 90)
+plt.ylabel("Daily New Cases")
+plt.title("Total Cases in the US")
 
 # plot
 plt.show()
