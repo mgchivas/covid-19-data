7056bc2da8c2d74e725681cb241d516d66caf8fa
Michael Guzman <mgchivas4@gmail.com>
2020-05-10 22:02:37 -0500

    Added 7 day averages

--- PlotCases.py
+++ PlotCases.py
@@ -9,18 +9,20 @@ statesPath = "us-states.csv"
 # County data
 countyData = pd.read_csv(countiesPath)
 cookMask = countyData["fips"] == 17031
-print(cookMask)
 countyDates = countyData[cookMask]["date"]
 countyCases = countyData[cookMask]["cases"].diff().fillna(0)
+county7DayAvg = countyCases.rolling(7).mean().fillna(0)
 
 # State data
 stateData = pd.read_csv(statesPath)
 ILMask = stateData["state"] == "Illinois"
 stateDates = stateData[ILMask]["date"]
 stateCases = stateData[ILMask]["cases"].diff().fillna(0)
+state7DayAvg = stateCases.rolling(7).mean().fillna(0)
 
 # plot county data
 plt.bar(countyDates, countyCases)
+plt.plot(countyDates, county7DayAvg)
 plt.xlabel("Date")
 plt.xticks(rotation = 90)
 plt.ylabel("Daily New Cases")
@@ -30,6 +32,7 @@ plt.figure() # create a new figure
 
 # plot state data
 plt.bar(stateDates, stateCases)
+plt.plot(stateDates, state7DayAvg)
 plt.xlabel("Date")
 plt.xticks(rotation = 90)
 plt.ylabel("Daily New Cases")
